138ec0163c44243f87f58352a1c4a7ef7ee25eab
Hendrik Ebbers <hendrik.ebbers@web.de>
2021-04-29 22:42:23 +0200

    new achievement
    (cherry picked from commit 748b71634f79f33daa7318e26384737146616ab0)

---
+++ src/main/java/eu/brundo/bot/achievements/TieVsBottiAchievment.java
@@ -0,0 +1,8 @@
+package eu.brundo.bot.achievements;
+
+public class TieVsBottiAchievment extends AbstractAchievment {
+
+    public TieVsBottiAchievment() {
+        super("tieVsBotti", 10);
+    }
+}
